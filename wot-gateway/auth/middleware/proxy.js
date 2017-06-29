84d8866f20a1cd7343d702092b9064b083737937
joelthierry <thierry.durot@ntb.ch>
2017-06-29 17:36:43 +0200

    Aenderungen, um App in Server zu integrieren

--- wot-gateway/auth/middleware/proxy.js
+++ wot-gateway/auth/middleware/proxy.js
@@ -4,8 +4,8 @@ var https = require('https'),
   httpProxy = require('http-proxy');
 var path = require('path');
 
-var useRedirects = true;
-exports.useRedirects = useRedirects;
+// var useRedirects = true;
+exports.useRedirects = true;
 
 var keyFilePath = path.join(__dirname, '..', 'config', 'privateKey.pem');
 var key_file = fs.readFileSync(keyFilePath, 'utf8');
