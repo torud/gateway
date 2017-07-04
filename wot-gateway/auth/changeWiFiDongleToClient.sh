1d8df21a82cbf72f79fd79cbcad71d3c4ede1d98
joelthierry <thierry.durot@ntb.ch>
2017-07-04 16:46:56 +0200

    Aenderungen, um App in Server zu integrieren

--- wot-gateway/auth/changeWiFiDongleToClient.sh
+++ wot-gateway/auth/changeWiFiDongleToClient.sh
@@ -1,1 +1,44 @@
-echo "Hello World"
+sudo service hostapd stop
+sudo service udhcpd stop
+sudo update-rc.d hostapd disable
+sudo update-rc.d udhcpd disable
+echo '# Defaults for hostapd initscript
+#
+# See /usr/share/doc/hostapd/README.Debian for information about alternative
+# methods of managing hostapd.
+#
+# Uncomment and set DAEMON_CONF to the absolute path of a hostapd configuration
+# file and hostapd will be started during system boot. An example configuration
+# file can be found at /usr/share/doc/hostapd/examples/hostapd.conf.gz
+#
+#DAEMON_CONF="/etc/hostapd/hostapd.conf"
+
+# Additional daemon options to be appended to hostapd command:-
+#       -d   show more debug messages (-dd for even more)
+#       -K   include key data in debug messages
+#       -t   include timestamps in some debug messages
+#
+# Note that -B (daemon mode) and -P (pidfile) options are automatically
+# configured by the init.d script and must not be added to DAEMON_OPTS.' 
+> '/etc/default/hostapd'
+
+echo '# interfaces(5) file used by ifup(8) and ifdown(8)
+# Include files from /etc/network/interfaces.d:
+source-directory /etc/network/interfaces.d
+
+allow-hotplug eth0
+        iface eth0 inet dhcp
+
+#allow-hotplug wlan0
+#iface wlan0 inet dhcp
+#        wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
+
+#allow-hotplug wlan0
+#iface wlan0 inet static
+#        address 192.168.2.1
+#        netmask 255.255.255.0'
+> '/etc/network/interfaces'
+
+sudo systemctl start NetworkManager.service
+sudo systemctl enable NetworkManager.service                        
+sudo nmcli r wifi on
