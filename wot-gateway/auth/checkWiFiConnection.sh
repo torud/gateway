7d61fb49824d0e2fc1230e485b0be215860b7894
joelthierry <thierry.durot@ntb.ch>
2017-07-18 13:49:20 +0200

    diverse Verbesserungen

--- wot-gateway/auth/checkWiFiConnection.sh
+++ wot-gateway/auth/checkWiFiConnection.sh
@@ -2,10 +2,11 @@
 
 echo "Hello"
 CONNECTIONS=$(nmcli dev)
-echo "${CONNECTIONS}"
-if [[ "$CONNECTIONS" =~ "unmanaged" ]]; then
-    echo "matched"
+# echo "${CONNECTIONS}"
+if [[ "$CONNECTIONS" =~ "disconnected" ]]; then
+    echo "disconnected, running changeWiFiDongleToHotspot.sh"
+    sh ./changeWiFiDongleToHotspot.sh
 else
-    echo "didn't match"
+    echo "connected"
 fi
 echo "Bye Bye"
