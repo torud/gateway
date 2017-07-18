b93f1018f1086cabeca9140a3eca7eeee6188a24
joelthierry <thierry.durot@ntb.ch>
2017-07-18 13:45:52 +0200

    diverse Verbesserungen

--- wot-gateway/auth/checkWiFiConnection.sh
+++ wot-gateway/auth/checkWiFiConnection.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "Hello"
-OUTPUT=$(nmcli dev)
-echo "${OUTPUT}"
+CONNECTIONS=$(nmcli dev)
+echo "${CONNECTIONS}"
+if [[ "$CONNECTIONS" =~ "unmanaged" ]]; then
+    echo "matched"
+else
+    echo "didn't match"
+fi
 echo "Bye Bye"
