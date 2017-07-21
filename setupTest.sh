87f4201e9e3a4560da9f2dd42ed3bee0e421f1aa
joelthierry <thierry.durot@ntb.ch>
2017-07-21 10:55:54 +0200

    diverse Verbesserungen

--- setupTest.sh
+++ setupTest.sh
@@ -3,12 +3,19 @@
 MDNS_NAME="kuengsauna"
 HOTSPOT_SSID="Sauna-Hotspot"
 HOTSPOT_PWD="kuengsaunaHotspotPassword1234"
+URL_GITHUB="https://github.com/joelthierry/"
+REPOSITORY="gateway"
 YALER_RELAIS_DOMAIN="gsiot-6p05-1srr"
 
-echo "setting up NanoPi NEO with mDNS-name $MDNS_NAME, hotspot SSID $HOTSPOT_SSID, hotspot password $PASSWORD and Yaler relais domain $YALER_RELAIS_DOMAIN"
+echo "setting up NanoPi NEO with mDNS-name $MDNS_NAME\n
+hotspot SSID $HOTSPOT_SSID\n
+hotspot password $PASSWORD\n
+github url $URL_GITHUB\n
+repository folder $REPOSITORY\n
+Yaler relais domain $YALER_RELAIS_DOMAIN"
 
 # setup network discovery with mDNS
 cat <<EOT > '/root/WoT/shellVariablesTest'
 127.0.0.1    localhost.localdomain localhost
-127.0.1.1    $MDNS_NAME
+127.0.1.1    $URL_GITHUB/$REPOSITORY
 EOT
