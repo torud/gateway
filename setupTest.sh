c5cadca9b8f7ecec147b0e25f88e902fe4d1e5c8
joelthierry <thierry.durot@ntb.ch>
2017-07-21 11:05:09 +0200

    diverse Verbesserungen

--- setupTest.sh
+++ setupTest.sh
@@ -3,19 +3,26 @@
 MDNS_NAME="kuengsauna"
 HOTSPOT_SSID="Sauna-Hotspot"
 HOTSPOT_PWD="kuengsaunaHotspotPassword1234"
+GITFOLDER="/root/WoT/"
 URL_GITHUB="https://github.com/joelthierry/"
-REPOSITORY="gateway"
+REPOSITORY="gateway/"
+AUTHSERVER_LOCATION="wot-gateway/auth/"
+WOTSERVER_LOCATION="wot-gateway/wot-server/"
 YALER_RELAIS_DOMAIN="gsiot-6p05-1srr"
 
-echo "setting up NanoPi NEO with mDNS-name $MDNS_NAME
-hotspot SSID $HOTSPOT_SSID
-hotspot password $PASSWORD
-github url $URL_GITHUB
-repository folder $REPOSITORY
-Yaler relais domain $YALER_RELAIS_DOMAIN"
+echo -e "setting up NanoPi NEO with following settings:
+mDNS-name\t$MDNS_NAME
+hotspot SSID\t$HOTSPOT_SSID
+hotspot password\t$PASSWORD
+folder to clone github in\t$GITFOLDER
+github url\t$URL_GITHUB
+repository folder\t$REPOSITORY
+auth-server folder\t$AUTHSERVER_LOCATION
+wot-server folder\t$WOTSERVER_LOCATION
+Yaler relais domain\t$YALER_RELAIS_DOMAIN"
 
 # setup network discovery with mDNS
 cat <<EOT > '/root/WoT/shellVariablesTest'
 127.0.0.1    localhost.localdomain localhost
-127.0.1.1    $URL_GITHUB$REPOSITORY
+127.0.1.1    $GITFOLDER$REPOSITORY$AUTHSERVER_LOCATION
 EOT
