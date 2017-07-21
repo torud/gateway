acfd150c3b9167f35987cbba60fa4251dc818d00
joelthierry <thierry.durot@ntb.ch>
2017-07-21 11:11:48 +0200

    diverse Verbesserungen

--- setupTest.sh
+++ setupTest.sh
@@ -13,7 +13,7 @@ YALER_RELAIS_DOMAIN="gsiot-6p05-1srr"
 echo -e "setting up NanoPi NEO with following settings:
 mDNS-name                       \t$MDNS_NAME
 hotspot SSID                    \t$HOTSPOT_SSID
-hotspot password                \t$PASSWORD
+hotspot password                \t$HOTSPOT_PWD
 folder to clone github in       \t$GITFOLDER
 github url                      \t$URL_GITHUB
 repository folder               \t$REPOSITORY
@@ -22,7 +22,7 @@ wot-server folder               \t$WOTSERVER_LOCATION
 Yaler relais domain             \t$YALER_RELAIS_DOMAIN"
 
 # setup network discovery with mDNS
-cat <<EOT > $GITFOLDERshellVariablesTest
+cat <<EOT > $GITFOLDER'shellVariablesTest'
 127.0.0.1    localhost.localdomain localhost
 127.0.1.1    $GITFOLDER$REPOSITORY$AUTHSERVER_LOCATION
 EOT
